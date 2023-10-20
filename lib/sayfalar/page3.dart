0478f8ae52fd38bbb658765a6967325329dc7a20
Alican Akdoğan <akdoganalican@gmail.com>
2023-10-20 19:10:40 +0300

    ayarlar sayfası eklendi

--- lib/sayfalar/page3.dart
+++ lib/sayfalar/page3.dart
@@ -12,9 +12,13 @@ class _page3State extends State<page3> {
   Future<List<Ayarlar>> tumAyarlariGoster() async {
     var ayarlarList = <Ayarlar>[];
 
-    var a1 = Ayarlar(1, "ayar_ad", "ayar_icon");
+    var a1 = Ayarlar(1, "Uygulamamızı Değerlendir", "star.png");
+    var a2 = Ayarlar(2, "Uygulama Sürümü", "information.png");
+    var a3 = Ayarlar(3, "İletişim", "contact.png");
 
     ayarlarList.add(a1);
+    ayarlarList.add(a2);
+    ayarlarList.add(a3);
 
     return ayarlarList;
 
@@ -32,11 +36,44 @@ class _page3State extends State<page3> {
         future: tumAyarlariGoster(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            var ayarlarListesi = snapshot.data;
-            return ListView(
-                //Listview tasarımı yapılacak
+            var ayarList = snapshot.data;
+            return ListView.builder(
+              itemCount: ayarList!.length,
+              itemBuilder: (context, index) {
+                var ayar = ayarList[index];
+                return GestureDetector(
+                  onTap: () {
+                    if (index == 0) {
+                      print("111");
+                    } else if (index == 1) {
+                      print("222");
+                    } else if (index == 2) {
+                      print("333");
+                    }
+                  },
+                  child: Card(
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Image.asset("resimler/${ayar.ayar_icon}"),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 1, 1, 1),
+                          child: Text("${ayar.ayar_ad}"),
+                        ),
+                      ],
+                    ),
+                  ),
                 );
+              },
+            );
           } else {
+            print("Veri Gelmedi");
             return Center();
           }
         },
