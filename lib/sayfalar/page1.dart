6b5d7ac7ac1e3365258e9b9907dfcb1ba7fe1b4a
Alican <akdoganalican@gmail.com>
2023-10-26 13:36:24 +0300

    fav icon button

--- lib/sayfalar/page1.dart
+++ lib/sayfalar/page1.dart
@@ -85,6 +85,7 @@ class _page1State extends State<page1> {
                                 ),
                               ),
                               IconButton(
+                                //Fav İcon button
                                 icon: Icon(Icons.favorite,
                                     color: isim.name_fav == '0'
                                         ? Colors.black
